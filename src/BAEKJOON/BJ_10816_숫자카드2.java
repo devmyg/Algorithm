e980bec0aa6abb0a0561787dcc295708b84e4fff
MooN <rankupkor@gmail.com>
2022-11-29 18:12:44 +0900

    2022-11-29 18:12

--- src/BAEKJOON/BJ_10816_숫자카드2.java
+++ src/BAEKJOON/BJ_10816_숫자카드2.java
@@ -6,21 +6,23 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
-public class BJ_1920_수찾기 {
+public class BJ_10816_숫자카드2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         HashMap<Integer, Integer> map = new HashMap();
+        int num;
 
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++) {
-            map.put(Integer.parseInt(st.nextToken()), 1);
+            num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
         n = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++) {
-            System.out.println(map.getOrDefault(Integer.parseInt(st.nextToken()), 0));
+            System.out.print(map.getOrDefault(Integer.parseInt(st.nextToken()), 0) + " ");
         }
     }
 }
