dd95e7838e332fc99de14e5121767c0088fc71ef
MooN <rankupkor@gmail.com>
2022-12-14 10:02:33 +0900

    2022-12-14 10:02

--- src/BAEKJOON/BJ_1463_1로만들기.java
+++ src/BAEKJOON/BJ_1463_1로만들기.java
@@ -5,21 +5,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BJ_1463_1로만들기 {
+    static Integer[] cache;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int cnt = 0;
+        cache = new Integer[n + 3];
+        cache[1] = 0;
+        cache[2] = cache[3] = 1;
+        System.out.println(recur(n));
+    }
 
-        System.out.print(n + " -> ");
-        while(n != 1) {
-            if(n % 3 == 0) n /= 3;
-            else if(n % 2 == 0) n /= 2;
-            else n--;
-            cnt++;
-            System.out.print(n + " -> ");
+    static int recur(int n) {
+        if(cache[n] == null) {
+            if(n % 6 == 0) cache[n] = Math.min(Math.min(recur(n / 3), recur(n / 2)), recur(n - 1)) + 1;
+            else if(n % 3 == 0) cache[n] = Math.min(recur(n / 3), recur(n - 1)) + 1;
+            else if(n % 2 == 0) cache[n] = Math.min(recur(n / 2), recur(n - 1)) + 1;
+            else cache[n] = recur(n - 1) + 1;
         }
-
-        System.out.println(cnt);
+        return cache[n];
     }
 }
